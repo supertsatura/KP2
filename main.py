36985b7c6b8654877b3aae9777bad5f26323e81d
supertsatura <davidtsaturyan@inbox.ru>
2026-03-22 14:17:53 +0300

    commit

--- main.py
+++ main.py
@@ -148,103 +148,102 @@ from itsdangerous import URLSafeTimedSerializer, BadSignature, SignatureExpired
 #     user = users.get(username)
 #     return user
 
-# 5.2
-app = FastAPI()
-SECRET_KEY = "my-secret-key"
-serializer = URLSafeTimedSerializer(SECRET_KEY)
-
-users = {
-    "David": {
-        "user_id": str(uuid.uuid4()),
-        "username": "Wfnehf",
-        "password": "12345",
-        "email": "wfnehf@example.com",
-        "full_name": "David Ts"
-    },
-    "Ivan": {
-        "user_id": str(uuid.uuid4()),
-        "username": "ivan123",
-        "password": "54321",
-        "email": "ivanov@example.com",
-        "full_name": "Ivan Ivanov"
-    }
-}
-
-username_to_user = {user["username"]: user for user in users.values()}
-
-
-class LoginRequestSchema(BaseModel):
-    username: str
-    password: str
-
-
-class UserProfileSchema(BaseModel):
-    user_id: str
-    username: str
-    full_name: str
-    email: str
-
-
-def verify_password(plain_password: str, stored_password: str) -> bool:
-    # проверка пароля
-    return plain_password == stored_password
-
-
-def create_signed_session_token(user_id: str) -> str:
-    # создание токена в формате <user_id>.<signature>
-    signature = serializer.dumps(user_id)
-    return f"{user_id}.{signature}"
-
-
-@app.post("/login", status_code=201, tags=["User"], summary="User authentication")
-async def create_user(response: Response, login_data: LoginRequestSchema):
-    user = username_to_user.get(login_data.username)
-
-    if not user or not verify_password(login_data.password, user["password"]):
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid username or password")
-
-    session_token = create_signed_session_token(user["user_id"])
-
-    response.set_cookie(
-        key="session_token",
-        value=session_token,
-        httponly=True,
-        max_age=3600,
-        samesite="lax"
-    )
-
-    return {
-        "message": "Login successful",
-        "username": login_data.username,
-        "session_token": session_token
-    }
-
-
-@app.get("/profile", status_code=200, response_model=UserProfileSchema, tags=["User"], summary="User profiling")
-async def get_profile(session_token: Annotated[str | None, Cookie()] = None):
-    if not session_token:
-        raise HTTPException(
-            status_code=401,
-            detail={"message": "Unauthorized"}
-        )
-    try:
-        user_id, signature = session_token.split(".", 1)
-        verified_user_id = serializer.loads(signature, max_age=3600)
-        if verified_user_id != user_id:
-            raise HTTPException(
-                status_code=401,
-                detail={"message": "Unauthorized"}
-            )
-        user = next((u for u in users.values() if u["user_id"] == user_id), None)
-        if not user:
-            raise HTTPException(
-                status_code=401,
-                detail={"message": "Unauthorized"}
-            )
-
-        return user
-    except (BadSignature, SignatureExpired):
-        raise HTTPException(status_code=401, detail="User unauthorized")
-
+# # 5.2
+# app = FastAPI()
+# SECRET_KEY = "my-secret-key"
+# serializer = URLSafeTimedSerializer(SECRET_KEY)
+#
+# users = {
+#     "David": {
+#         "user_id": str(uuid.uuid4()),
+#         "username": "Wfnehf",
+#         "password": "12345",
+#         "email": "wfnehf@example.com",
+#         "full_name": "David Ts"
+#     },
+#     "Ivan": {
+#         "user_id": str(uuid.uuid4()),
+#         "username": "ivan123",
+#         "password": "54321",
+#         "email": "ivanov@example.com",
+#         "full_name": "Ivan Ivanov"
+#     }
+# }
+#
+# username_to_user = {user["username"]: user for user in users.values()}
+#
+#
+# class LoginRequestSchema(BaseModel):
+#     username: str
+#     password: str
+#
+#
+# class UserProfileSchema(BaseModel):
+#     user_id: str
+#     username: str
+#     full_name: str
+#     email: str
+#
+#
+# def verify_password(plain_password: str, stored_password: str) -> bool:
+#     # проверка пароля
+#     return plain_password == stored_password
+#
+#
+# def create_signed_session_token(user_id: str) -> str:
+#     # создание токена в формате <user_id>.<signature>
+#     signature = serializer.dumps(user_id)
+#     return f"{user_id}.{signature}"
+#
+#
+# @app.post("/login", status_code=201, tags=["User"], summary="User authentication")
+# async def create_user(response: Response, login_data: LoginRequestSchema):
+#     user = username_to_user.get(login_data.username)
+#
+#     if not user or not verify_password(login_data.password, user["password"]):
+#         raise HTTPException(
+#             status_code=401,
+#             detail="Invalid username or password")
+#
+#     session_token = create_signed_session_token(user["user_id"])
+#
+#     response.set_cookie(
+#         key="session_token",
+#         value=session_token,
+#         httponly=True,
+#         max_age=3600,
+#         samesite="lax"
+#     )
+#
+#     return {
+#         "message": "Login successful",
+#         "username": login_data.username,
+#         "session_token": session_token
+#     }
+#
+#
+# @app.get("/profile", status_code=200, response_model=UserProfileSchema, tags=["User"], summary="User profiling")
+# async def get_profile(session_token: Annotated[str | None, Cookie()] = None):
+#     if not session_token:
+#         raise HTTPException(
+#             status_code=401,
+#             detail={"message": "Unauthorized"}
+#         )
+#     try:
+#         user_id, signature = session_token.split(".", 1)
+#         verified_user_id = serializer.loads(signature, max_age=3600)
+#         if verified_user_id != user_id:
+#             raise HTTPException(
+#                 status_code=401,
+#                 detail={"message": "Unauthorized"}
+#             )
+#         user = next((u for u in users.values() if u["user_id"] == user_id), None)
+#         if not user:
+#             raise HTTPException(
+#                 status_code=401,
+#                 detail={"message": "Unauthorized"}
+#             )
+#
+#         return user
+#     except (BadSignature, SignatureExpired):
+#         raise HTTPException(status_code=401, detail="User unauthorized")
